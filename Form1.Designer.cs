f03b5e0759ec52ea89e0fa3ec14e2e568614f036
al-metal <al-metal@users.noreply.github.com>
2017-07-05 10:49:32 +0400

    авторизация  нетхауз поиск товаров рейсер
    Авторизация на нетхаузе
    Поиск в списке товара с артикулом R00

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.ofdOpenFile = new System.Windows.Forms.OpenFileDialog();
             this.btnOpen = new System.Windows.Forms.Button();
             this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPasswords = new System.Windows.Forms.TextBox();
+            this.tbLogin = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // ofdOpenFile
@@ -57,16 +61,55 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Пароль:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Логин:";
+            // 
+            // tbPasswords
+            // 
+            this.tbPasswords.Location = new System.Drawing.Point(12, 93);
+            this.tbPasswords.Name = "tbPasswords";
+            this.tbPasswords.Size = new System.Drawing.Size(125, 20);
+            this.tbPasswords.TabIndex = 14;
+            this.tbPasswords.UseSystemPasswordChar = true;
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(12, 54);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(125, 20);
+            this.tbLogin.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(556, 360);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPasswords);
+            this.Controls.Add(this.tbLogin);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.btnOpen);
             this.Name = "Form1";
             this.Text = "Обработать title";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +118,10 @@
         private System.Windows.Forms.OpenFileDialog ofdOpenFile;
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPasswords;
+        private System.Windows.Forms.TextBox tbLogin;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using OfficeOpenXml;
+﻿using Bike18;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,8 @@ namespace EditTitliBike18
     public partial class Form1 : Form
     {
         string fileUrlsAllProducts;
+
+        nethouse nethouse = new nethouse();
 
 
         public Form1()
@@ -30,12 +34,23 @@ namespace EditTitliBike18
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            Properties.Settings.Default.login = tbLogin.Text;
+            Properties.Settings.Default.password = tbPasswords.Text;
+            Properties.Settings.Default.Save();
+
             if (ofdOpenFile.FileName == "openFileDialog1" || ofdOpenFile.FileName == "")
             {
                 MessageBox.Show("Ошибка при выборе файла", "Ошибка файла");
                 return;
             }
             fileUrlsAllProducts = ofdOpenFile.FileName.ToString();
+
+            CookieContainer cookie = nethouse.CookieNethouse(tbLogin.Text, tbPasswords.Text);
+            if (cookie.Count == 1)
+            {
+                MessageBox.Show("Логин или пароль для сайта введены не верно", "Ошибка логина/пароля");
+                return;
+            }
 
             FileInfo file = new FileInfo(fileUrlsAllProducts);
             ExcelPackage p = new ExcelPackage(file);
@@ -45,9 +60,27 @@ namespace EditTitliBike18
             for (int i = 2; q > i; i++)
             {
                 string url = (string)w.Cells[i, 2].Value;
+
+                if (url.Contains("category"))
+                    continue;
+
+                List<string> tovarB18 = nethouse.GetProductList(cookie, url);
+
+                string article = tovarB18[6];
+                if (article.Contains("R00"))
+                {
+
+                }
+
             }
 
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbLogin.Text = Properties.Settings.Default.login;
+            tbPasswords.Text = Properties.Settings.Default.password;
         }
     }
 }
